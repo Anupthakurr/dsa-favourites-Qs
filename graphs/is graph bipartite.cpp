cf1bca02a62f72757b4dea7d4a6f1dcfd05dfe53
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-06-29 18:03:42 +0530

    Update is graph bipartite.cpp

--- graphs/is graph bipartite.cpp
+++ graphs/is graph bipartite.cpp
@@ -71,3 +71,38 @@ public:
           return true;
             }
 };
+
+
+odd length and even length wali approach using bfs
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> level(n, -1); // -1 means unvisited
+
+        for (int i = 0; i < n; i++) {
+            if (level[i] == -1) {
+                queue<int> q;
+                q.push(i);
+                level[i] = 0; // Start at level 0
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int neighbor : graph[node]) {
+                        if (level[neighbor] == -1) {
+                            level[neighbor] = level[node] + 1;
+                            q.push(neighbor);
+                        } else if (level[neighbor]  == level[node] ) {
+                            // Same level parity → odd-length cycle
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true; // No odd-length cycles
+    }
+};
