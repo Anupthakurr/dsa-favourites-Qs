cc4fa2314ca7c929225924080941e2d21f244123
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-06-29 20:37:29 +0530

    Update amazon hackon solution .cpp

--- graphs/amazon hackon solution .cpp
+++ graphs/amazon hackon solution .cpp
@@ -2,7 +2,8 @@
 
 
 
- 
+qs --> 1 solution
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
@@ -113,3 +114,67 @@ int main()
     solve();
   }
 }
+
+
+
+------------------------------------------*------*---------------------------------------------------------------------->
+qs 2 -----> solution
+
+
+ #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t; // number of test cases
+
+  while (t--)
+  {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+
+    int b_val;
+    cin >> b_val;
+    set<int> b = {b_val};
+
+    sort(a.begin(), a.end());
+
+    long long ans = 0;
+
+    for (int val : a)
+    {
+      if (b.count(val - 1) && b.count(1))
+      {
+        ans += 3 * (val - 1);
+      }
+      else if (b.count(val - 1) || b.count(1))
+      {
+        if (b.count(val - 1))
+        {
+          b.insert(1);
+        }
+        else
+        {
+          b.insert(val - 1);
+        }
+        ans += 2 * (val - 1);
+      }
+      else
+      {
+        b.insert(1);
+        b.insert(val - 1);
+        ans += (val - 1);
+      }
+    }
+
+    cout << ans << endl;
+  }
+
+  return 0;
+}
+
