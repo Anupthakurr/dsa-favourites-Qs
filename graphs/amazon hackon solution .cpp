fc68e94c2f2631473df8d5440be2119796bdf18d
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-06-29 20:33:47 +0530

    Create amazon hackon solution .cpp

---
+++ graphs/amazon hackon solution .cpp
@@ -0,0 +1,116 @@
+
+
+
+
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+  int n, m;
+  cin >> n >> m;
+  vector<pair<int, int>> edges(m);
+  for (auto &it : edges)
+  {
+    cin >> it.first >> it.second;
+  }
+  int k;
+  cin >> k;
+  vector<int> v(k);
+  for (auto &it : v)
+  {
+    cin >> it;
+  }
+  vector<vector<int>> adj(n);
+  for (auto &it : edges)
+  {
+    adj[it.first - 1].push_back(it.second - 1);
+  }
+
+  int ans = 0;
+  bool flag = true;
+
+  vector<int> dist(n, 1e7);
+  dist[0] = 0;
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  pq.push({0, 0});
+  while (!pq.empty())
+  {
+    auto temp = pq.top();
+    pq.pop();
+    int node = temp.second;
+    int d = temp.first;
+    for (auto it : adj[node])
+    {
+      if (dist[it] > d + 1)
+      {
+        dist[it] = d + 1;
+        pq.push({d + 1, it});
+      }
+    }
+  }
+  for (int j = 0; j < k; j++)
+  {
+    ans += dist[v[j] - 1];
+  }
+
+  //    now shortest dist from mountain to  city 1
+
+  for (int m = 0; m < k; m++)
+  {
+    vector<int> dist(n, 1e7);
+    dist[v[m] - 1] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, v[m] - 1});
+    while (!pq.empty())
+    {
+      auto temp = pq.top();
+      pq.pop();
+      int node = temp.second;
+      int d = temp.first;
+      for (auto it : adj[node])
+      {
+        if (dist[it] > d + 1)
+        {
+          dist[it] = d + 1;
+          pq.push({d + 1, it});
+        }
+      }
+    }
+    ans += dist[0];
+    if (dist[0] == 1e7)
+    {
+      flag = false;
+    }
+  }
+  for (int l = 0; l < k; l++)
+  {
+    if (dist[v[l] - 1] == 1e7)
+    {
+      flag = false;
+    }
+  }
+  if (flag == false)
+  {
+    cout << -1 << endl;
+  }
+  else
+  {
+    cout << ans << endl;
+  }
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
