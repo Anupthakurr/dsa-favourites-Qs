3c290565a0156b7ebf09f362933782a3a05a958e
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-07-01 10:48:16 +0530

    Update sieve of erasthothenes.cpp

--- math/sieve of erasthothenes.cpp
+++ math/sieve of erasthothenes.cpp
@@ -1,1 +1,26 @@
+qs link --->  https://leetcode.com/problems/find-the-count-of-numbers-which-are-not-special/description/
 
+
+
+class Solution {
+public:
+    int nonSpecialCount(int l, int r) {
+        int temp1=sqrt(r);
+        int cnt=0;
+        vector<bool>prime(temp1+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i<=temp1;i++){
+            if(i*i<l || i*i>r){
+                prime[i]=false;
+            }
+            if(prime[i]){
+                cnt++;
+            }
+                for(int j=i*i;j<=temp1;j=j+i){
+                    prime[j]=0;
+                
+            }
+        }
+        return((r-l+1)-cnt);
+    }   
+};
