da845143e886907992232583d39b73e3e342dcd3
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-07-03 09:05:48 +0530

    Create koko eating bananas.cpp

---
+++ binary search/koko eating bananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool check(int mid,vector<int>& piles, int h){
+         long long sum = 0;
+         for(int j = 0;j<piles.size();j++){
+                sum += ceil((double)piles[j]/mid);
+            }
+        if(sum <= h){
+           return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+       int end = *max_element(piles.begin(), piles.end());;
+       int start = 1;
+       int ans ;
+       while(start<=end){
+        int mid = (start + end)/2;
+           if(check(mid,piles,h)){
+            end = mid -1;
+            ans = mid;
+           } 
+           else{
+            start = mid+1;
+           }
+       }
+       return ans;
+    }
+};
