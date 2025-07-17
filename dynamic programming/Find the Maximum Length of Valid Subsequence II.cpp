12fda77f9966c2ea48cd217d27848f32fe561067
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-07-17 19:40:51 +0530

    Update Find the Maximum Length of Valid Subsequence II.cpp

--- dynamic programming/Find the Maximum Length of Valid Subsequence II.cpp
+++ dynamic programming/Find the Maximum Length of Valid Subsequence II.cpp
@@ -23,3 +23,50 @@ public:
         return maxSub;
     }
 };
+
+______________________________________________________________________________________________________________________
+top down approach
+
+class Solution {
+public:
+    int n, k;
+    vector<vector<int>> dp;
+    vector<int> nums;
+
+    int check(int i, int prev) {
+        if (i == n) return 0;
+
+        if (dp[i][prev + 1] != -1)
+            return dp[i][prev + 1];
+
+        int take = 0, notTake = 0;
+
+        if (prev == -1) {
+            notTake = check(i + 1, prev);
+
+            for (int j = i + 1; j < n; ++j) {
+                take = max(take, 2 + check(j, (nums[i] + nums[j]) % k));
+            }
+        } else {
+            for (int j = i + 1; j < n; ++j) {
+                if ((nums[i] + nums[j]) % k == prev) {
+                    take = 1 + check(j, prev);
+                    break;
+                }
+            }
+        }
+
+        return dp[i][prev + 1] = max(take, notTake);
+    }
+
+    int maximumLength(vector<int>& nums_, int k_) {
+        nums = nums_;
+        k = k_;
+        n = nums.size();
+        dp.assign(n, vector<int>(k + 1, -1));
+        return check(0, -1);
+    }
+};
+
+
+
