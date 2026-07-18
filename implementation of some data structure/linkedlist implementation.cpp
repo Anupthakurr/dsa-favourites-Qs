efca4484fa6285aaf921b39c7986c78a4192e620
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2026-07-19 02:19:09 +0530

    Update linkedlist implementation.cpp

--- implementation of some data structure/linkedlist implementation.cpp
+++ implementation of some data structure/linkedlist implementation.cpp
@@ -161,3 +161,165 @@ void deleteNode(int position, Node* & head) {
 
     return 0;
 }
+
+// different implementation
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+    }
+
+    ~Node() {
+        cout << "Deleting node " << data << endl;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    ~LinkedList() {
+        Node* curr = head;
+
+        while (curr != nullptr) {
+            Node* nextNode = curr->next;
+            delete curr;
+            curr = nextNode;
+        }
+    }
+
+    void insertAtHead(int data) {
+        Node* newNode = new Node(data);
+
+        if (head == nullptr) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void insertAtTail(int data) {
+        Node* newNode = new Node(data);
+
+        if (head == nullptr) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void insertAtPosition(int pos, int data) {
+
+        if (pos <= 1) {
+            insertAtHead(data);
+            return;
+        }
+
+        Node* curr = head;
+        int cnt = 1;
+
+        while (curr != nullptr && cnt < pos - 1) {
+            curr = curr->next;
+            cnt++;
+        }
+
+        if (curr == nullptr || curr == tail) {
+            insertAtTail(data);
+            return;
+        }
+
+        Node* newNode = new Node(data);
+
+        newNode->next = curr->next;
+        curr->next = newNode;
+    }
+
+    void deleteAtPosition(int pos) {
+
+        if (head == nullptr)
+            return;
+
+        if (pos == 1) {
+
+            Node* temp = head;
+
+            head = head->next;
+
+            if (head == nullptr)
+                tail = nullptr;
+
+            delete temp;
+            return;
+        }
+
+        Node* prev = nullptr;
+        Node* curr = head;
+
+        int cnt = 1;
+
+        while (curr != nullptr && cnt < pos) {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        if (curr == nullptr)
+            return;
+
+        prev->next = curr->next;
+
+        if (curr == tail)
+            tail = prev;
+
+        delete curr;
+    }
+
+    void print() {
+
+        Node* temp = head;
+
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+
+        cout << endl;
+    }
+};
+
+int main() {
+
+    LinkedList list;
+
+    list.insertAtHead(10);
+    list.insertAtTail(20);
+    list.insertAtTail(30);
+    list.insertAtPosition(2, 15);
+
+    list.print();
+
+    list.deleteAtPosition(3);
+
+    list.print();
+
+    return 0;
+}
