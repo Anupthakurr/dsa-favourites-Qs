32cbda6732a5b72e96d7765df36a83e647c25c82
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2026-07-20 22:26:05 +0530

    Add stack implementation with basic operations
    Implemented a stack data structure with push, pop, peek, isEmpty, getSize, and print methods.

--- implementation of some data structure/stack implementation.cpp
+++ implementation of some data structure/stack implementation.cpp
@@ -116,3 +116,100 @@ int main() {
 
     return 0;
 }
+
+//  better implementation of stack
+
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int *arr;
+    int top;
+    int size;
+
+public:
+    Stack(int s) {
+        size = s;
+        arr = new int[size];
+        top = -1;
+    }
+
+    ~Stack() {
+        delete[] arr;
+    }
+
+    void push(int data) {
+        if (top == size - 1) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+
+        top++;
+        arr[top] = data;
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+
+        top--;
+    }
+
+    int peek() {
+        if (top == -1) {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+
+        return arr[top];
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    int getSize() {
+        return top + 1;
+    }
+
+    void print() {
+        if (isEmpty()) {
+            cout << "Stack is Empty" << endl;
+            return;
+        }
+
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+
+    Stack st(5);
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    st.print();
+
+    cout << "Top Element : " << st.peek() << endl;
+
+    st.pop();
+
+    st.print();
+
+    cout << "Current Size : " << st.getSize() << endl;
+
+    if (st.isEmpty())
+        cout << "Stack is Empty" << endl;
+    else
+        cout << "Stack is Not Empty" << endl;
+
+    return 0;
+}
