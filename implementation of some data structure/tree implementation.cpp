5b74116b057471f6674bf7d5c45b20383a6db240
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2026-07-21 20:53:54 +0530

    Update tree implementation.cpp

--- implementation of some data structure/tree implementation.cpp
+++ implementation of some data structure/tree implementation.cpp
@@ -166,3 +166,83 @@ int main() {
 
     return 0;
 }
+
+
+
+//  different implementation
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+void inorder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+
+    // Creating the binary tree
+    Node* root = new Node(10);
+
+    root->left = new Node(20);
+    root->right = new Node(30);
+
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+
+    cout << "Inorder: ";
+    inorder(root);
+
+    cout << "\nPreorder: ";
+    preorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    delete root; // Deletes the entire tree
+
+    return 0;
+}
